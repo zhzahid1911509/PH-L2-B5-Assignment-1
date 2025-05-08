95caf72b1003a33b0fd8a7a418fc79b1a61c7670
Zahid Hasan <zahid.hasan.nsu2019@gmail.com>
2025-05-08 20:58:00 +0600

    solved problem 4

--- Assignment_1.ts
+++ Assignment_1.ts
@@ -41,3 +41,32 @@ function concatenateArrays<T>(...arrays: T[][]): T[]{
 
     return concatenatedArray;
 }
+
+//
+
+class Vehicle {
+    private make: string;
+    private year: number;
+
+    public constructor(make:string, year: number){
+        this.make = make;
+        this.year = year;
+    }
+
+    public getInfo():string {
+        return `Make: ${this.make}, Year: ${this.year}`;
+    }
+}
+
+class Car extends Vehicle {
+    private model: string;
+
+    public constructor(make:string, year: number, model:string){
+        super(make, year);
+        this.model = model;
+    }
+
+    public getModel(){
+        return `Model: ${this.model}`;
+    }
+}
