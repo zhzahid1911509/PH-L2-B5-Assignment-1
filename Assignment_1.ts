6884689a4ac54c4f0fb7a2dbf8cc67b0452fbf14
Zahid Hasan <zahid.hasan.nsu2019@gmail.com>
2025-05-08 20:15:19 +0600

    solved problem 2

--- Assignment_1.ts
+++ Assignment_1.ts
@@ -8,3 +8,22 @@ function formatString(input: string, toUpper?: boolean): string {
 
     return input;
 }
+
+//
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    const filteredItems: {
+        title: string;
+        rating: number;
+    }[] = [];
+
+    for( let i = 0; i<items.length; i++ ){
+        if(items[i].rating >= 4){
+            filteredItems.push(items[i]);
+        }
+    }
+
+    return filteredItems;
+}
+
+//
