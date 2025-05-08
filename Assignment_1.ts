22065c2e430892af4926dacf5368fb24ac258f28
Zahid Hasan <zahid.hasan.nsu2019@gmail.com>
2025-05-08 21:33:47 +0600

    solved problem 7

--- Assignment_1.ts
+++ Assignment_1.ts
@@ -107,3 +107,26 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
 
 //
 
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  function getDayType(day: Day): string{
+    switch(day){
+        case Day.Saturday:
+        case Day.Sunday:
+            return "Weekend";
+        default:
+            return "Weekday";
+    }
+  }
+
+  //
+
+  
